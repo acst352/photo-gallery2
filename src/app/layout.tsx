cd651fcb3cd6dc7cfa5ce5766a829006f87a4fcc
Alex <acst352@gmail.com>
2024-12-01 23:13:41 -0600

    update dir structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { montserrat } from "@/config";
+import { montserrat } from "@/src/config";
 import "./globals.css";
 
 export const metadata: Metadata = {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { ArrayPhotos } from "@/components";
+import { ArrayPhotos } from "@/src/components";
 
 export default async function Home() {
   return <ArrayPhotos />;
