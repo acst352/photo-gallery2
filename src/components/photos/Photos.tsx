b88342cd23a1ebfe4e292af74af574d8d840195f
Alex <acst352@gmail.com>
2024-12-10 00:13:36 -0600

    Desde abajo y contrabajo...

--- src/components/photos/Photos.tsx
+++ src/components/photos/Photos.tsx
@@ -1,37 +1,20 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
+import { getPhotos } from '../../services/photo/getPhotos';
+
 export const Photos = () => {
+  // const getPhotos = async() => {
+  //   try {
+  //     const photos = await getPhotos()
+  //     setPhotos(photos)
+      
+  //   }
+  // }
   return (
     <>
       <div className="photos px-8 w-full p-8">
         Photos
-        <div className="w-full h-screen">
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores,
-            ab nemo quam aliquam architecto similique consectetur? Nihil unde
-            deserunt necessitatibus, esse, beatae nisi, illum non recusandae qui
-            aliquid dolor numquam voluptatibus nesciunt consequatur. Eveniet,
-            nemo perspiciatis? Beatae velit porro distinctio soluta suscipit,
-            magnam fugit ipsa adipisci voluptate ea, explicabo voluptatibus
-            vitae minus est delectus laudantium repellat eum tempora non? Odio
-            perspiciatis quam fugit reprehenderit similique at, ad deserunt
-            culpa laboriosam inventore fuga molestias a vitae alias earum,
-            accusamus ullam aperiam porro explicabo. Magni repudiandae
-            distinctio cum libero fuga, facilis totam magnam ipsam nihil minus.
-            Eum, unde reprehenderit. A, fugiat ratione? Lorem ipsum, dolor sit
-            amet consectetur adipisicing elit. Maiores, ab nemo quam aliquam
-            architecto similique consectetur? Nihil unde deserunt
-            necessitatibus, esse, beatae nisi, illum non recusandae qui aliquid
-            dolor numquam voluptatibus nesciunt consequatur. Eveniet, nemo
-            perspiciatis? Beatae velit porro distinctio soluta suscipit, magnam
-            fugit ipsa adipisci voluptate ea, explicabo voluptatibus vitae minus
-            est delectus laudantium repellat eum tempora non? Odio perspiciatis
-            quam fugit reprehenderit similique at, ad deserunt culpa laboriosam
-            inventore fuga molestias a vitae alias earum, accusamus ullam
-            aperiam porro explicabo. Magni repudiandae distinctio cum libero
-            fuga, facilis totam magnam ipsam nihil minus. Eum, unde
-            reprehenderit. A, fugiat ratione?
-          </p>
-        </div>
+        <div className="w-full h-screen"></div>
       </div>
     </>
   );
