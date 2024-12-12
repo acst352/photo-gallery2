15a5725943df5174859e6e0e56f8223600a74584
Alex <acst352@gmail.com>
2024-12-11 20:05:33 -0600

    centrar imagen

--- src/components/header/Photos.tsx
+++ src/components/header/Photos.tsx
@@ -3,8 +3,11 @@ import React from "react";
 export const Photos = () => {
   return (
     <div className="container w-full h-screen justify-center">
-      <img src={"https://us-tuna-sounds-images.voicemod.net/3f451aa1-6491-4d94-8cc7-1510255c4af6-1720710831792.png"}>
-      </img>
+      <img
+        src="https://us-tuna-sounds-images.voicemod.net/3f451aa1-6491-4d94-8cc7-1510255c4af6-1720710831792.png"
+        alt="Description of the image"
+        className="max-w-sm h-auto object-cover mx-auto"
+      />
     </div>
   );
 };
